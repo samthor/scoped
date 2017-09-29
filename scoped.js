53e3d6d7e15fd277e603d40e2c013eafc407de4e
Sam Thorogood <sam.thorogood@gmail.com>
2017-09-29 13:32:09 +1000

    initial

---
+++ scoped.js
@@ -0,0 +1,229 @@
+/**
+ * @fileoverview Polyfill for `<style scoped>`.
+ */
+
+(function() {
+  const s = document.createElement('style');
+  if ('scoped' in s) {
+    return;  // do nothing
+  }
+
+  Object.defineProperty(HTMLStyleElement.prototype, 'scoped', {
+    enumerable: true,
+    get() {
+      return this.hasAttribute('scoped');
+    },
+    set(v) {
+      if (v) {
+        this.setAttribute('scoped', this.getAttribute('scoped') || '');
+      } else {
+        this.removeAttribute('scoped');
+      }
+    },
+  });
+
+  /**
+   * @type {!WeakMap<!HTMLStyleElement, *>}
+   */
+  const styleNodes = new WeakMap();
+
+
+
+
+  function hashCode(s) {
+    let hash = 5381;
+    let j = s.length;
+    while (j) {
+      hash = (hash * 33) ^ s.charCodeAt(--j);
+    }
+    return hash;
+  }
+
+
+
+  /**
+   * @param {!CSSStyleSheet} sheet
+   * @param {string} prefix
+   */
+  const upgradeSheet = (function() {
+
+    /**
+     * @type {!WeakMap<!StyleSheet, string>}
+     */
+    const upgradedSheets = new WeakMap();
+
+   /**
+    * @type {!Map<!CSSImportRule, string>}
+    */
+    const pendingImportRule = new Map();
+
+    /**
+     * @param {!CSSRule} rule
+     * @return {Node} owner of rule
+     */
+    function ownerNode(rule) {
+      let sheet = rule.parentStyleSheet;
+      while (sheet) {
+        if (sheet.ownerNode) {
+          return sheet.ownerNode;
+        }
+        sheet = sheet.parentStyleSheet;
+      }
+      return null;
+    }
+
+    /**
+     * Callback inside rAF to monitor for sheet loading.
+     */
+    function process() {
+      pendingImportRule.forEach((prefix, importRule) => {
+        if (importRule.styleSheet) {
+          internalUpgrade(importRule.styleSheet, prefix);
+        } else if (ownerNode(importRule)) {
+          return;  // still valid, do nothing
+        }
+        pendingImportRule.delete(importRule);
+      });
+      // check again next frame
+      pendingImportRule.size && window.requestAnimationFrame(process);
+    }
+
+    /**
+     * @param {!CSSStyleSheet} sheet already loaded CSSStyleSheet
+     * @param {string} prefix to apply
+     */
+    function internalUpgrade(sheet, prefix) {
+      if (upgradedSheets.get(sheet) === prefix) {
+        return;  // already done
+      }
+      upgradedSheets.set(sheet, prefix);
+
+      const l = sheet.rules.length;
+      for (let i = 0; i < l; ++i) {
+        const rule = sheet.rules[i];
+
+        if (rule instanceof CSSImportRule) {
+          if (rule.styleSheet) {
+            // TODO: recursion is bad
+            internalUpgrade(rule.styleSheet, prefix);
+            continue;
+          }
+
+          // otherwise, add to pending queue
+          if (!pendingImportRule.size) {
+            window.requestAnimationFrame(process);
+          }
+          pendingImportRule.set(rule, prefix)
+
+        } else if (rule instanceof CSSStyleRule) {
+          rule.selectorText = prefix + rule.selectorText;
+        } else {
+          console.warning('unhandled rule', rule);
+        }
+      }
+    }
+
+    return internalUpgrade;
+  }());
+
+
+  /**
+   * @param {!HTMLStyleElement} node to reset
+   */
+  function resetCSS(node) {
+    const css = node.textContent;
+    node.textContent = '';
+    node.textContent = css;
+  }
+
+
+  let uniqueId = 0;
+
+
+  function upgrade(node) {
+    let state = styleNodes.get(node);
+    if (state) {
+      if (!node.scoped) {
+        styleNodes.delete(node);
+        resetCSS(node);  // force the browser to reparse this CSS
+      } else if (node.sheet) {
+        // nb. node.sheet is null if being removed
+        upgradeSheet(node.sheet, state.prefix);
+      }
+
+      const effectiveParent = node.scoped && document.body.contains(node) ? node.parentNode : null;
+      if (state.parent !== effectiveParent) {
+        if (state.parent) {
+          state.parent.removeAttribute(state.attrName);
+        }
+        if (effectiveParent) {
+          effectiveParent.setAttribute(state.attrName, '');
+        }
+        state.parent = effectiveParent;
+        console.info('style', state.attrName, 'now on', effectiveParent);
+      }
+
+      return false;  // already upgraded
+    }
+
+    if (!node.scoped) {
+      return;
+    }
+
+    // TODO: use hash for deduping
+    const hash = hashCode(node.textContent);
+
+    const attrName = `__scope_${++uniqueId}`
+    const prefix = `[${attrName}] `;
+
+    upgradeSheet(node.sheet, prefix);
+
+    node.parentNode.setAttribute(attrName, '');
+    styleNodes.set(node, {attrName, prefix, parent: node.parentNode});
+  }
+
+
+  function resolve(changes, opt_dirty) {
+    console.info('got potential changes', [...changes]);
+
+    [...changes].forEach(upgrade);
+  }
+
+  const mo = new MutationObserver((records) => {
+    const changes = new Set();
+    const iterate = (nodes) => {
+      let i = nodes ? nodes.length : 0;
+      while (i) {
+        const node = nodes[--i];
+        if (!(node instanceof HTMLElement)) {
+          continue;
+        }
+        if (node instanceof HTMLStyleElement) {
+          changes.add(node);
+          continue;
+        }
+        const cand = node.querySelectorAll('style');
+        let j = cand.length;
+        while (j) {
+          changes.add(cand[--j]);
+        }
+      }
+    };
+
+    records.forEach((record) => {
+      if (record.target instanceof HTMLStyleElement) {
+        changes.add(record.target);
+      } else {
+        iterate(record.addedNodes);
+        iterate(record.removedNodes);
+      }
+    });
+
+    resolve(changes);
+  });
+
+  const options = {childList: true, subtree: true, attributes: true, attributeFilter: ['scoped']};
+  mo.observe(document.body, options);
+  resolve(document.querySelectorAll('style[scoped]'));
+
+}());
